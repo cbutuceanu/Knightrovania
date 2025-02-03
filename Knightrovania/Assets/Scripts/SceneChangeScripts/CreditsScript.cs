03bf52d415158c6b5eb837c51c0af64c2ba2e473
Zachary Templin <149111073+TheVaultMancer@users.noreply.github.com>
2025-02-02 20:34:01 -0500

    Credits Scene/Scene Change Buttons
    I Added optional buttons to go to a credits Scene as well as imported the skeleton anims I made, made scripts (that were partially included in last push) that take you from one scene to another, and added buttons to work in tandem with the scripts to multiple scenes.

---
+++ Knightrovania/Assets/Scripts/SceneChangeScripts/CreditsScript.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditsScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void CreditsTP()
+    {
+        SceneManager.LoadScene("Credits");
+
+
+    }
+}
